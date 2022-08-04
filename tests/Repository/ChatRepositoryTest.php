47bcbeb0b50dba31facb7843f24a7faf72074a97
shavkat.aminov <sh.aminov@ssdsoftwaresolutions.com>
2022-08-05 00:40:31 +0500

    add additional tests

--- tests/Repository/ChatRepositoryTest.php
+++ tests/Repository/ChatRepositoryTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Repository;
 
 use App\Entity\Chat;
+use App\Entity\Message;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -18,6 +19,7 @@ class ChatRepositoryTest extends KernelTestCase
         $this->entityManager = $kernel->getContainer()
             ->get('doctrine')
             ->getManager();
+
     }
 
     public function testAdd() {
@@ -31,12 +33,56 @@ class ChatRepositoryTest extends KernelTestCase
         $chatFind = $this->entityManager->getRepository(Chat::class)->findOneBy(['first_user' => 1, 'second_user' => 2]);
 
         $this->assertEquals($chat, $chatFind);
+        $this->assertNotNull($chat->getCreatedAt());
+        $this->assertNotNull($chat->getUpdatedAt());
     }
+
+
+    public function testRemove() {
+        $chat = new Chat();
+        $chat->setFirstUser(1);
+        $chat->setSecondUser(2);
+
+        $this->entityManager->getRepository(Chat::class)
+            ->remove($chat, true);
+
+        $chatFind = $this->entityManager->getRepository(Chat::class)->findOneBy(['first_user' => 1, 'second_user' => 2]);
+
+        $this->assertNull($chatFind);
+    }
+
+    public function testCreateMessageAndChatIfNeeded() {
+
+        $this->entityManager->getRepository(Chat::class)
+            ->createMessageAndChatIfNeeded(1, 2, "testing message");
+
+        $chatFind = $this->entityManager->getRepository(Chat::class)->findOneBy(['first_user' => 1, 'second_user' => 2]);
+
+        $this->assertNotNull($chatFind);
+        if($chatFind) {
+            $message = $this->entityManager->getRepository(Message::class)->findOneBy(['chat_id' => $chatFind->getId()]);
+            $this->assertNotNull($message);
+            $this->assertEquals($message->getContent(), 'testing message');
+        }
+
+        $this->entityManager->getRepository(Chat::class)
+            ->createMessageAndChatIfNeeded(1, 2, "message");
+
+        $chatFind = $this->entityManager->getRepository(Chat::class)->findBy(['first_user' => 1, 'second_user' => 2]);
+        $this->assertNotNull($chatFind);
+        $this->assertEquals(count($chatFind), 1);
+    }
+
+
 
     protected function tearDown(): void
     {
+        $this->entityManager->getRepository(Chat::class)
+            ->createQueryBuilder('c')
+            ->delete()
+            ->getQuery()
+            ->execute();
         parent::tearDown();
-
         // doing this is recommended to avoid memory leaks
         $this->entityManager->close();
         $this->entityManager = null;
---
+++ tests/Repository/MessageRepositoryTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MessageRepositoryTest extends KernelTestCase
+{
+
+}
